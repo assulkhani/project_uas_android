bdcde10defa3036f7a5dde11b7a567e023d9e3ae
assulkhani <assulkhani08@gmail.com>
2019-07-12 22:47:13 +0700

    first commit project uas android

---
+++ app/src/main/java/com/assulkhani/project_uas_android/models/ComplainRealm.kt
@@ -0,0 +1,5 @@
+package com.assulkhani.project_uas_android.models
+
+open class ComplainRealm {
+
+}
---
+++ app/src/main/java/com/assulkhani/project_uas_android/utils/Utilities.kt
@@ -0,0 +1,9 @@
+package com.assulkhani.project_uas_android.utils
+
+class Utilities {
+    companion object {
+        fun getCurrentToken() : String? {
+            return null
+        }
+    }
+}
